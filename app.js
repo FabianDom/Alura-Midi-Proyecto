50321b3f9cad1c842b2439d1fb6aae0624955830
FabianDom <fabiandmz13@gmail.com>
2024-10-10 10:15:20 -0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-function hola () {
-        let audioPom = document.getElementById('sonido_tecla_pom').play();
-   
-}
+// creamos una funcion para el sonido de las teclas y le agregamos un parametro que vamos hacer dinamico para cada sonido.
 
-document.querySelector('.tecla_pom').onclick = hola
+function sonidoTecla(audioTeclaId) {
+  document.getElementById(audioTeclaId).play();
+}
+// obtener lista de la clase tecla
+const listaTeclas = document.querySelectorAll(".tecla");
+// recorrer la lista mediante un bucle While
+for (let index = 0; index < listaTeclas.length; index++) {
+        let audioId = `sonido_${listaTeclas[index].classList[1]}`
+        listaTeclas[index].onclick = function () {
+          sonidoTecla(audioId);
+  }  
+        
+}
+  
